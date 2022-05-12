3cd80d30e6569866b77b5288255f1d88f9b6143b
Miryung Yeon <miryung9023@naver.com>
2022-05-13 01:30:14 +0900

    LifeCycle API

--- src/Counter.js
+++ src/Counter.js
@@ -1,33 +1,83 @@
-import { Component } from 'react';
-import React from "react";
+import React, { Component } from 'react';
+
+const Problematic = () => {
+    throw (new Error('버그가 나타났다!'));
+    return (
+      <div>
+        
+      </div>
+    );
+  };  
 
 class Counter extends Component {
-    state = {
-        number : 0
-    }
+  state = {
+    number: 0
+  }
 
-    handleIncrease = () => {
-        this.setState({
-            number : this.state.number + 1
-        })
-    }
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
 
-    handleDecrease = () => {
-        this.setState({
-            number : this.state.number - 1
-        })
-    }
+  componentDidCatch(error, info) {
+    this.setState({
+      error: true
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <h1> 카운터 </h1>
-                <div> 값 : {this.state.number}</div>
-                <button onClick={this.handleIncrease}>+</button>
-                <button onClick={this.handleDecrease}>-</button>
-            </div>
-        )
-    }
+  componentWillMount() {
+    console.log('componentWillMount (deprecated)');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // 5 의 배수라면 리렌더링 하지 않음
+    console.log('shouldComponentUpdate');
+    if (nextState.number % 5 === 0) return false;
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('componentWillUpdate');
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate');
+  }
+  
+
+  handleIncrease = () => {
+    const { number } = this.state;
+    this.setState({
+      number: number + 1
+    });
+  }
+
+  handleDecrease = () => {
+    this.setState(
+      ({ number }) => ({
+        number: number - 1
+      })
+    );
+  }
+  
+  render() {
+
+    if (this.state.error) return (<h1>에러발생!</h1>);
+    
+    return (
+      <div>
+        <h1>카운터</h1>
+        <div>값: {this.state.number}</div>
+        { this.state.number === 4 && <Problematic /> }
+        <button onClick={this.handleIncrease}>+</button>
+        <button onClick={this.handleDecrease}>-</button>
+      </div>
+    );
+  }
 }
 
 export default Counter;
